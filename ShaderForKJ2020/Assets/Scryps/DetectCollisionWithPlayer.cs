15ff00c2602d4b652e7ab2b1a2686496462c36fa
ZROS <zros98@gmail.com>
2020-02-02 10:44:20 +0200

    Add hit and portal sound

--- ShaderForKJ2020/Assets/Scryps/DetectCollisionWithPlayer.cs
+++ ShaderForKJ2020/Assets/Scryps/DetectCollisionWithPlayer.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class DetectCollisionWithPlayer : MonoBehaviour
 {
+    public AudioSource hit;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             OurGameManager.actualState = OurGameController.GameState.end;
-        Debug.Log(OurGameManager.actualState );
+            Debug.Log(OurGameManager.actualState );
+            hit.Play();
+        }
+           
     }
 
     private void OnTriggerStay(Collider other)
     {
-       if(other.CompareTag("Player")) 
-           OurGameManager.actualState = OurGameController.GameState.end;
-       Debug.Log(OurGameManager.actualState );
+        if (other.CompareTag("Player"))
+        {
+            OurGameManager.actualState = OurGameController.GameState.end;
+            Debug.Log(OurGameManager.actualState );
+        }
+           
     }
 }
