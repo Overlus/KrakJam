06ec1d7c2eec6a2c122a13c89f898b2be33d8958
Mikolaj <twardowskimikolaj@interia.pl>
2020-02-02 10:43:45 +0100

    Animacja Wybuchu postaci

---
+++ ShaderForKJ2020/Assets/Scryps/Twardy/PlayerDestroyAnimationScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestroyAnimationScript : MonoBehaviour
+{
+    [SerializeField] GameObject parent;
+    [SerializeField] Rigidbody[] childrenToLunchOut;
+
+
+    void Start()
+    {
+        childrenToLunchOut = GetComponentsInChildren<Rigidbody>();
+        Camera.main.transform.position = transform.position + (Vector3.up * 2 - Vector3.forward * 3);
+        Camera.main.transform.LookAt(transform);
+
+        parent.GetComponent<MeshRenderer>().enabled = false;
+
+        StartCoroutine(WaitABit());
+
+
+    }
+
+
+    IEnumerator WaitABit()
+    {
+
+        yield return new WaitForSeconds(0.7f);
+
+        foreach (Rigidbody rig in childrenToLunchOut)
+        {
+            rig.isKinematic = false;
+            rig.AddExplosionForce(200, transform.position, 1.5f);
+        }
+
+
+    }
+
+    // Update is called once per frame
+
+}
